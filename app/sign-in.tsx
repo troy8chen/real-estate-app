19569a3c479e3cdd96c6ce573abb83e982bac2f3
troy8chen <troy8chen@gmail.com>
2025-01-13 20:19:48 -0800

    Sign-In Page

--- app/sign-in.tsx
+++ app/sign-in.tsx
@@ -1,11 +1,44 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Image } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import images from '@/constants/images'
+import icons from '@/constants/icons'
 
 const SignIn = () => {
+  const handleLogin = () => {}
+
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className='bg-white h-full'>
+      <ScrollView contentContainerStyle={{ height: '100%' }}>
+        <Image source={images.onboarding} className="w-full h-4/6" resizeMode="contain" />
+        <View className='px-10'>
+          <Text className='text-base text-center uppercase font-rubik text-black-200'>
+            Welcome to ReState
+          </Text>
+          <Text className='text-3xl font-rubik-bold text-black-300 text-center mt-2'> 
+            Let's Get You Closer to {"\n"}
+            <Text className='text-primary-300'>
+              Your Ideal Home
+            </Text>
+          </Text>
+          <Text className='text-lg font-rubik text-black-200 text-center mt-12'>
+            Login to ReState with Google
+          </Text>
+          <TouchableOpacity onPress={handleLogin} className='bg-white shadow-zinc-300  rounded-full
+          w-full py-4 mt-5'></TouchableOpacity>
+          <View className='flex flex-row items-center justify-center'>
+            <Image 
+                source={icons.google}
+                className='w-5 h-5'
+                resizeMode='contain'
+            />
+            <Text className='text-lg font-rubik-medium text-black-300 ml-2'>
+                Continue with Google
+            </Text>
+          </View>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
